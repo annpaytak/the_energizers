604b961f41dd840fab09be75d4239c8cfbc2b23f
mac <mac@Annas-MacBook-Pro.local>
2023-03-10 15:46:35 +0200

    fix footer icons

--- assets/OurMission-a3d6475c.js
+++ assets/OurMission-a3d6475c.js
@@ -1,9 +1,9 @@
-import{a as e,j as n,S as o,n as i,c as a,h as s,d as t,b as r,f as d}from"./index-bc3ec08d.js";function g(){return e(c,{children:n(l,{children:[n(o,{content:"02",right:"14vw",children:["Our ",e("br",{}),"Mission"]}),n(m,{children:[e(p,{children:"We aim to build meaningful friendship with like-minded people."}),e(h,{children:"Together we synergize, we exchange energy and experience. So that we can energize each other, inspary and create something powerful and unique."})]})]})})}const c=i.section`
+import{j as e,g as n,S as o,n as i,d as a,h as s,i as t,e as r,f as d}from"./index-f4a535f1.js";function u(){return e(l,{children:n(m,{children:[n(o,{content:"02",right:"14vw",children:["Our ",e("br",{}),"Mission"]}),n(c,{children:[e(p,{children:"We aim to build meaningful friendship with like-minded people."}),e(g,{children:"Together we synergize, we exchange energy and experience. So that we can energize each other, inspary and create something powerful and unique."})]})]})})}const l=i.section`
   height: 100%;
   position: relative;
   display: flex;
   background-color: ${a.pink};
-`,l=i.div`
+`,m=i.div`
   width: ${s};
   margin: auto;
   margin-top: ${t};
@@ -17,7 +17,7 @@ import{a as e,j as n,S as o,n as i,c as a,h as s,d as t,b as r,f as d}from"./ind
     grid-template-columns: 1.2fr 1fr;
     grid-template-rows: auto;
   }
-`,m=i.div`
+`,c=i.div`
   width: 100%;
   margin-top: 2rem;
 
@@ -37,6 +37,6 @@ import{a as e,j as n,S as o,n as i,c as a,h as s,d as t,b as r,f as d}from"./ind
   margin-bottom: 1.2rem;
 
   text-transform: uppercase;
-`,h=i.p`
+`,g=i.p`
   margin: 0;
-`;export{g as OurMission};
+`;export{u as OurMission};
